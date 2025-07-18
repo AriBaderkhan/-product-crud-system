3801a52dd320dfb92d0eeede59260ad8fd630334
Ari Baderkhan <aribaderkhan@gmail.com>
2025-07-18 14:52:56 +0300

    Fix some errs

--- storage/store.js
+++ storage/store.js
@@ -2,19 +2,16 @@ const fs = require('fs');
 const path = require('path');
 
 
-const filePath = path.join(__dirname,"../data/products.json");
+const filePath = path.join(__dirname,'../data','products.json');
+
+function readProduct(){
+    if (!fs.existsSync(filePath)) return [];
+    const data = fs.readFileSync(filePath,'utf-8')
+    return JSON.parse(data)
+}
 
 function writeProduct(products){
     fs.writeFileSync(filePath,JSON.stringify(products,null,2))
-}
-
-
-function readProduct(){
-    if (!fs.existsSync(filePath)) {
-        fs.writeFileSync(filePath,[])
-    }
-    const data = fs.readFileSync(filePath,'utf-8')
-    return JSON.parse(data)
 }
 
 module.exports = { readProduct , writeProduct }
