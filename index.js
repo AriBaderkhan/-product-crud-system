108d2be0ba294a521bdb3b37069d0bd05b400d16
Ari Baderkhan <aribaderkhan@gmail.com>
2025-07-29 19:35:19 +0300

    Add security best practices: Helmet, CORS, sanitization, SQL protection, and rate limiting

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const helmet = require('helmet');
+const cors = require('cors');
+// const xss = require('xss-clean');
 
 const productRoutes = require('./routes/productRoutes');
 const studentRoutes = require('./routes/studentRoutes');
@@ -10,7 +13,12 @@ const { emitter, logMorgan } = require("./logger_system");
 app.use(morgan("combined", { stream: logMorgan }));
 
 const PORT = 1234;
+app.use(helmet());
+app.use(cors());
+// app.use(xss());
 app.use(express.json());
+
+
 
 app.use('/products',productRoutes); // the core for product
 app.use('/students',studentRoutes); // the core for student
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const router = express.Router();
-const {authControllerRegister , authControllerLogin}  = require('../controllers/authController')
-const {authValidateRegister , authValidateLogin} = require('../validations/authValidate')
-const verifyToken = require('../middlewares/authMiddleware')
-const {checkRole} = require('../middlewares/roleMiddleware')
+const {authControllerRegister , authControllerLogin}  = require('../controllers/authController');
+const {authValidateRegister , authValidateLogin} = require('../validations/authValidate');
+const verifyToken = require('../middlewares/authMiddleware');
+const {checkRole} = require('../middlewares/roleMiddleware');
+
+const rateLimit =  require('express-rate-limit');
+const loginLimiter = rateLimit({
+  windowMs: 30 * 1000, // 15 seconds
+  max: 3, // Max 3 requests per windowMs
+  message: {
+    status: 429,
+    message: '⛔ Too many requests, slow down!',
+  },
+});
 
 router.post('/register',authValidateRegister , authControllerRegister)
-router.post('/login',authValidateLogin , authControllerLogin)
+router.post('/login',loginLimiter,authValidateLogin , authControllerLogin)
 
 router.get('/adminDashboard',verifyToken ,checkRole('Admin'), (req,res)=>{  // test for protectiong path
     const user = req.user.name
